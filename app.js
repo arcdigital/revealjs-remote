44ff1a02325b94de9d9b1d8fd9ac0d81b9cd0528
Anand Capur <me@anand.io>
2014-03-04 19:49:11 -0500

    Added remote stuff

--- app.js
+++ app.js
@@ -12,3 +12,33 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.get('/', function (req, res) {
 	res.redirect('/index.html');
 });
+
+
+io.set('loglevel', 10)
+
+io.on('connection', function(socket) {
+	console.log('shit worked');
+
+  socket.on('swipe-right', function(data) {
+		console.log('right');
+		socket.broadcast.emit('swipe-right', data);
+	});
+
+	socket.on('swipe-left', function(data) {
+		console.log('left');
+		socket.broadcast.emit('swipe-left', data);
+	});
+
+	socket.on('swipe-up', function(data) {
+		console.log('up');
+		socket.broadcast.emit('swipe-up', data);
+	});
+
+	socket.on('swipe-down', function(data) {
+		console.log('down');
+		socket.broadcast.emit('swipe-down', data);
+	});
+
+
+	//socket.broadcast.emit('swipe-right');
+});
--- public/js/remote.js
+++ public/js/remote.js
@@ -1,17 +1,46 @@
 $(function () {
 	var previous = { h: 0, v: 0 };
 
+var socket = io.connect();
+
+socket.on('swipe-left', function (data) {
+	console.log(data);
+	if (data.h !== previous.h || data.v !== previous.v)
+	Reveal.left();
+});
+socket.on('swipe-right', function (data) {
+	console.log(data);
+	if (data.h !== previous.h || data.v !== previous.v)
+	Reveal.right();
+});
+socket.on('swipe-up', function (data) {
+	console.log(data);
+	if (data.h !== previous.h || data.v !== previous.v)
+	Reveal.up();
+});
+socket.on('swipe-down', function (data) {
+	console.log(data);
+	if (data.h !== previous.h || data.v !== previous.v)
+	Reveal.down();
+});
+
 	Reveal.addEventListener('slidechanged', function (event) {
-		if (event.indexh > previous.h) {
-			console.log('swipe-right');
-		} else if (event.indexh < previous.h) {
-			console.log('swipe-left');
-		} else if (event.indexv > previous.v) {
-			console.log('swipe-down');
-		} else if (event.indexv < previous.v) {
-			console.log('swipe-up');
+		var next = { h: event.indexh, v: event.indexv };
+
+		if (next.h > previous.h) {
+			console.log('swipe-right1');
+			socket.emit('swipe-right', next);
+		} else if (next.h < previous.h) {
+			console.log('swipe-left2');
+			socket.emit('swipe-left', next);
+		} else if (next.v > previous.v) {
+			console.log('swipe-down3');
+			socket.emit('swipe-down', next);
+		} else if (next.v < previous.v) {
+			console.log('swipe-up4');
+			socket.emit('swipe-up', next);
 		}
-		previous = { h: event.indexh, v: event.indexv };
+		previous = next;
 	});
 
 });
